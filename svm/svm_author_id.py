0fff3e9b0ad1c2f1f0d2000866cc26d7fc8b9125
Wei Liao <ericliaowei@gmail.com>
2016-01-05 01:18:15 -0800

    finish mini-project 2

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -24,6 +24,28 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
+#clf = SVC(kernel="linear")
+
+clf = SVC(kernel='rbf', C=10000)
+
+# select a smaller training set
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(labels_test, pred)
+print "Accuracy is: ", accuracy
+
+# predict for element 10 of the test set? The 26th? The 50th?
+print "10th element: ", pred[10], \
+      "\n26th element: ", pred[26], \
+      "\n50th element: ", pred[50]
+
+# There are over 1700 test events: how many are predicted to be in the "Chris" (1) class
+print """Number predicted to be in the "Chris" class is:""", sum(pred == 1)
 
 #########################################################
 
